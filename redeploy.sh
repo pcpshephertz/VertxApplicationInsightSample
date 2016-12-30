b87aac3c8a9d551bb04affbc99e9311ff9a5db26
Prakash <prakash.pandey@shephertz.com>
2016-12-30 18:41:25 +0530

    pom updated
    Signed-off-by: Prakash <prakash.pandey@shephertz.com>

--- redeploy.sh
+++ redeploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export LAUNCHER="io.vertx.core.Launcher"
-export VERTICLE="io.vertx.starter.MainVerticle"
+export VERTICLE="app42.vertx.starter.Main"
 export CMD="mvn compile"
 export VERTX_CMD="run"
 
--- src/main/java/app42/vertx/starter/MainVertex.java
+++ src/main/java/app42/vertx/starter/MainVertex.java
@@ -3,10 +3,12 @@ package app42.vertx.starter;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class Main {
+public class MainVertex {
 	
 	public static void main(String[] args) {
 		ExecutorService service = Executors.newFixedThreadPool(10);
 		service.submit(new Controller());
+		
+		System.out.println("Server started....!");
 	}
 }
